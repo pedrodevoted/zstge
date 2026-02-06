35495a766991e5bd2c56046c2e0e79aa77c84265
pedrodevoted <ks1s9d@gmail.com>
2026-02-06 12:09:24 -0300

    Chore: add display helpers

---
+++ display.py
@@ -0,0 +1,56 @@
+class display:
+    gi = "\033[3;90m"
+
+    black = "\033[0;30m"
+    red = "\033[0;31m"
+    bred = "\033[1;31m"
+    green = "\033[0;32m"
+    bgreen = "\033[1;32m"
+    yellow = "\033[0;33m"
+    byellow = "\033[1;33m"
+    blue = "\033[0;34m"
+    bblue = "\033[1;34m"
+    purple = "\033[0;35m"
+    bpurple = "\033[1;35m"
+    cyan = "\033[0;36m"
+    bcyan = "\033[1;36m"
+    white = "\033[0m"
+    bwhite = "\033[1;37m"
+    lwhite = "\033[5;37m"
+
+    banner3 = '''
+                  ########        ########
+                  ########        ########
+              ################################
+            ####################################
+            ####################################
+          ########################################
+        ##########################################
+        ##          ####################        ####
+      ##              ################            ####
+      ##                ####    ####                ##
+    ####                ####    ####                ##
+      ##                ############                ##
+      ##              ####  ##    ##                ##
+      ####            ##            ##            ##
+        ####      ####              ######    ######
+            ########                    ########
+    '''
+# Instância global para usar direto
+bwhite = display.bwhite
+byellow = display.byellow
+yellow = display.yellow
+white = display.white
+red = display.red
+green = display.green
+bred = display.bred
+bgreen = display.bgreen
+bblue = display.bblue
+blue = display.blue
+purple = display.purple
+bpurple = display.bpurple
+cyan = display.cyan
+bcyan = display.bcyan
+gi = display.gi
+lwhite = display.lwhite
+banner3 = display.banner3
